0f314e5391c92aa9e92badfd89b122af5380dd26
cmwslw <cwalker32@gmail.com>
2009-09-19 13:43:37 -0700

    added comments

--- src/all/freeze.asm
+++ src/all/freeze.asm
@@ -1,8 +1,8 @@
 MOV r3, #0xd3
-MSR CPSR_c, r3
+MSR CPSR_c, r3            @ Supervisor mode, no IRQs, no FIQs
 MOV r3, #0
 MOV r2, #0x3c800000
 MOV r1, #0xa5
-STR r1, [r2]
+STR r1, [r2]              @ Shoot watchdog
 SUBS r1, r1, #1
 B 0x18
